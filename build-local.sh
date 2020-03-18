8783989e2ff885184395ce1c868c969a153dd113
Drsdre <github.com@drsdre.org>
2020-03-18 21:46:42 +0100

    Add php 7.4 containers

--- build-local.sh
+++ build-local.sh
@@ -5,3 +5,5 @@ docker build . -f 7.2/alpine/cli/Dockerfile -t drsdre/php-laravel:7.2-cli-alpine
 docker build . -f 7.2/alpine/fpm/Dockerfile -t drsdre/php-laravel:7.2-fpm-alpine
 docker build . -f 7.3/alpine/cli/Dockerfile -t drsdre/php-laravel:7.3-cli-alpine
 docker build . -f 7.3/alpine/fpm/Dockerfile -t drsdre/php-laravel:7.3-fpm-alpine
+docker build . -f 7.4/alpine/cli/Dockerfile -t drsdre/php-laravel:7.4-cli-alpine
+docker build . -f 7.4/alpine/fpm/Dockerfile -t drsdre/php-laravel:7.4-fpm-alpine
