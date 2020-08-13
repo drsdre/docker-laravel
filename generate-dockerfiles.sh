1cf0df4f69815f100f2b52fb3597ff68e5695cb3
Drsdre <github.com@drsdre.org>
2020-08-13 10:55:19 +0200

    Add container for fpm and nginx combined. Changed conf.d directory to config. Install acpu extension. Run supervisord for combined fpm-nginx under www-data.

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-VERSION=7.2 TYPE=cli OS=alpine ./templater.sh Dockerfile > 7.2/alpine/cli/Dockerfile
-VERSION=7.2 TYPE=fpm OS=alpine ./templater.sh Dockerfile > 7.2/alpine/fpm/Dockerfile
-VERSION=7.3 TYPE=cli OS=alpine ./templater.sh Dockerfile > 7.3/alpine/cli/Dockerfile
-VERSION=7.3 TYPE=fpm OS=alpine ./templater.sh Dockerfile > 7.3/alpine/fpm/Dockerfile
-VERSION=7.3 TYPE=fpm OS=alpine ./templater.sh Dockerfile_fpm-nginx > 7.3/alpine/fpm-nginx/Dockerfile
-VERSION=7.4 TYPE=cli OS=alpine ./templater.sh Dockerfile > 7.4/alpine/cli/Dockerfile
-VERSION=7.4 TYPE=fpm OS=alpine ./templater.sh Dockerfile > 7.4/alpine/fpm/Dockerfile
-VERSION=7.4 TYPE=fpm OS=alpine ./templater.sh Dockerfile_fpm-nginx > 7.4/alpine/fpm-nginx/Dockerfile
+VERSION=7.2 TYPE=cli OS=alpine ./templater.sh Dockerfile_cli.tmpl > 7.2/alpine/cli/Dockerfile
+VERSION=7.2 TYPE=fpm OS=alpine ./templater.sh Dockerfile_fpm.tmpl > 7.2/alpine/fpm/Dockerfile
+VERSION=7.3 TYPE=cli OS=alpine ./templater.sh Dockerfile_cli.tmpl > 7.3/alpine/cli/Dockerfile
+VERSION=7.3 TYPE=fpm OS=alpine ./templater.sh Dockerfile_fpm.tmpl > 7.3/alpine/fpm/Dockerfile
+VERSION=7.3 TYPE=fpm OS=alpine ./templater.sh Dockerfile_fpm-nginx.tmpl > 7.3/alpine/fpm-nginx/Dockerfile
+VERSION=7.4 TYPE=cli OS=alpine ./templater.sh Dockerfile_cli.tmpl > 7.4/alpine/cli/Dockerfile
+VERSION=7.4 TYPE=fpm OS=alpine ./templater.sh Dockerfile_fpm.tmpl > 7.4/alpine/fpm/Dockerfile
+VERSION=7.4 TYPE=fpm OS=alpine ./templater.sh Dockerfile_fpm-nginx.tmpl > 7.4/alpine/fpm-nginx/Dockerfile
