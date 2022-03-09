ffe33eecc19a5401bc7d2ab22ba1bb89fd4784f8
Drsdre <github.com@drsdre.org>
2022-03-09 13:54:16 +0100

    Remove fpm-nginx image.

--- build-local.sh
+++ build-local.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 docker build . -f 7.4/alpine/cli/Dockerfile -t drsdre/php-laravel:7.4-cli-alpine \
 && docker build . -f 7.4/alpine/fpm/Dockerfile -t drsdre/php-laravel:7.4-fpm-alpine \
-&& docker build . -f 7.4/alpine/fpm-nginx/Dockerfile -t drsdre/php-laravel:7.4-fpm-nginx-alpine \
 && docker build . -f 8.0/alpine/cli/Dockerfile -t drsdre/php-laravel:8.0-cli-alpine \
 && docker build . -f 8.0/alpine/fpm/Dockerfile -t drsdre/php-laravel:8.0-fpm-alpine \
-&& docker build . -f 8.0/alpine/fpm-nginx/Dockerfile -t drsdre/php-laravel:8.0-fpm-nginx-alpine \
 && docker build . -f 8.1/alpine/cli/Dockerfile -t drsdre/php-laravel:8.1-cli-alpine \
-&& docker build . -f 8.1/alpine/fpm/Dockerfile -t drsdre/php-laravel:8.1-fpm-alpine \
-&& docker build . -f 8.1/alpine/fpm-nginx/Dockerfile -t drsdre/php-laravel:8.1-fpm-nginx-alpine
+&& docker build . -f 8.1/alpine/fpm/Dockerfile -t drsdre/php-laravel:8.1-fpm-alpine
--- push-docker.sh
+++ push-docker.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 docker push drsdre/php-laravel:7.4-cli-alpine \
 && docker push drsdre/php-laravel:7.4-fpm-alpine \
-&& docker push drsdre/php-laravel:7.4-fpm-nginx-alpine \
 && docker push drsdre/php-laravel:8.0-cli-alpine \
 && docker push drsdre/php-laravel:8.0-fpm-alpine \
-&& docker push drsdre/php-laravel:8.0-fpm-nginx-alpine \
 && docker push drsdre/php-laravel:8.1-cli-alpine \
-&& docker push drsdre/php-laravel:8.1-fpm-alpine \
-&& docker push drsdre/php-laravel:8.1-fpm-nginx-alpine
+&& docker push drsdre/php-laravel:8.1-fpm-alpine
