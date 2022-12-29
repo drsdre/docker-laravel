d8f0e196b34def74c493523b247b66cccb4129a8
Drsdre <github.com@drsdre.org>
2022-12-29 09:27:10 +0100

    Add PHP 8.2. Use install-php-extensions through container. Remove fmp-nginx template.

--- push-docker.sh
+++ push-docker.sh
@@ -4,4 +4,6 @@ docker push drsdre/php-laravel:7.4-cli-alpine \
 && docker push drsdre/php-laravel:8.0-cli-alpine \
 && docker push drsdre/php-laravel:8.0-fpm-alpine \
 && docker push drsdre/php-laravel:8.1-cli-alpine \
-&& docker push drsdre/php-laravel:8.1-fpm-alpine
+&& docker push drsdre/php-laravel:8.1-fpm-alpine \
+&& docker push drsdre/php-laravel:8.2-cli-alpine \
+&& docker push drsdre/php-laravel:8.2-fpm-alpine
